cade27d85fef96a59947ca0b5f332f10e1f61a65
Azuesflip <azuesflip@gmail.com>
2015-06-11 21:25:17 -0600

    inititalizing repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KB::Application.config.secret_key_base = '957ebfa493d64d4c4429428cbceefc7a136200ee444687d327570cc2dcd8cb6a76073732790f08f04be9d28d07809c159d077d78a9781b4269bd27ac9d67a663'
